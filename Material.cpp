f2bb1f15de77051afd5318cc59e8efd44fad50c7
Oriol <oornaqbl13@alumnes.ub.edu>
2020-05-04 18:51:29 +0200

    class Material

--- Material.cpp
+++ Material.cpp
@@ -2,7 +2,15 @@
 
 
 Material::Material() {
+    this->diffuse = vec3(1.0f);
+}
 
+Material::Material(vec3 a, vec3 d, vec3 s, float alpha, float beta) {
+    diffuse = d;
+    ambient = a;
+    specular = s;
+    this->alpha = alpha; // Transparencia 1.0f
+    this->beta = beta; // Shineness
 }
 
 /**
@@ -11,5 +19,9 @@ Material::Material() {
  * @param program
  */
 void Material::toGPU(QGLShaderProgram *program){
+}
+
+vec3 Material::getDiffuse(vec2 point) const{
+    return this->diffuse;
 }
 
--- Material.h
+++ Material.h
@@ -3,6 +3,8 @@
 
 #include <QGLShaderProgram>
 
+#include "library/Common.h"
+
 // TO DO: A canviar a la fase 1 de la practica 2
 // Classe que representa els materials d'un objecte
 class Material {
@@ -10,8 +12,17 @@ class Material {
 
 public:
     Material();
+    Material(vec3 a, vec3 d, vec3 s, float alpha, float beta);
 
     void toGPU(QGLShaderProgram *program);
+
+    vec3 getDiffuse(vec2 point) const;
+
+    vec3 diffuse;
+    vec3 ambient;
+    vec3 specular;
+    float alpha;
+    int beta;
 
 };
 
