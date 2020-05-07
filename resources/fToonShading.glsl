fafe069f0a38605a38c32b82e81d4693e700862c
Oriol <oornaqbl13@alumnes.ub.edu>
2020-05-07 19:27:04 +0200

    fToonShading multiple lights

--- resources/fToonShading.glsl
+++ resources/fToonShading.glsl
@@ -9,28 +9,37 @@ struct light {
     vec3 ambient, diffuse, specular;
     vec3 atenuacio;
     vec4 position, direction;
+    int type;
 };
 
-uniform light dir_light;
+uniform light lights[20];
 
 void main()
 {
-    float r = dot(normalize(dir_light.direction), normal);
-
-    float v1 = 3/4;
-    float v2 = 2/4;
-    float v3 = 1/4;
-
     vec3 color = vec3(1);
 
-    if(r >= v3)
-        color *= 1;
-    else if(r >= v2)
-        color *= v3;
-    else if(r >= v1)
-        color *= v2;
-    else
-        color *= v1;
+    for(int i = 0; i < 20; ++i)
+    {
+        if(lights[i].type == 1) // 1==DIRECTIONAL
+        {
+            float r = dot(normalize(lights[i].direction), normal);
+
+            float v1 = 3/4;
+            float v2 = 2/4;
+            float v3 = 1/4;
+
+            if(r >= v3)
+                color *= 1;
+            else if(r >= v2)
+                color *= v3;
+            else if(r >= v1)
+                color *= v2;
+            else
+                color *= v1;
+
+            break;  // compute with the first directional light found and exit
+        }
+    }
 
     colorOut = vec4(color, 1);
 }
