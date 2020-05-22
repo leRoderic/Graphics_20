5a3d0ee6d37f4719d1558e8d9b65482966565fc6
leRoderic from ShitBuntu <rcmikado@gmail.com>
2020-05-22 19:31:25 +0200

    data

--- Builder.cpp
+++ Builder.cpp
@@ -1,5 +1,7 @@
 #include "Builder.h"
 
+#include <objects/plane.h>
+
 Builder::Builder(GLWidget *glWid)
 {
     glWidget = glWid;
@@ -44,7 +46,20 @@ void Builder::newDataScene(QString data, QString obj, QString colormap,
 {
     // TO DO Fase 1: Crear la nova escena a partir de les dades
     // de la finestra de visualization mapping corresponent
-    qDebug() << xmin;
+    vec4 v1, v2, v3, v4;
+    v1 = vec4(xmin, ymin, zmin, 1);
+    v2 = vec4(xmax, ymin, zmin, 1);
+    v3 = vec4(xmin, ymin, zmax, 1);
+    v4 = vec4(xmax, ymin, zmax, 1);
+    Plane *p = new Plane(v1, v2, v3, v4);
+    //scene = new Scene();
+    p->setTexture(new QOpenGLTexture(QImage("://resources/textures/2k_venus_surface.jpg")));
+    scene->addObject(p);
+    Light *light = new Light(vec4(10, 20, 20,0), vec4(0.0f),vec3(0.2f), vec3(0.8f), vec3(1.0f), vec3(0.0f), Puntual);
+    //light->setTipusLight(Puntual);
+    scene->addLight(light);
+    //DataReader *dr = new DataReader(scene);
+    //dr->xMax = xmax;
     emit newScene(scene);
 }
 
--- objects/plane.cpp
+++ objects/plane.cpp
@@ -1,31 +1,29 @@
 #include "plane.h"
 #include <cmath>
 
-Plane::Plane(vec4 v1, vec4 v2, vec4 v3, vec4 v4):Object(6, NULL){
+Plane::Plane(vec4 v1, vec4 v2, vec4 v3, vec4 v4):Object(6, nullptr){
 
     this->points[0] = v1;
     this->points[1] = v2;
     this->points[2] = v3;
     this->points[3] = v4;
-    this->points[4] = v2;
-    this->points[5] = v3;
-
-    this->normals[0] = vec4(0,1,0,0);
-    this->normals[1] = vec4(0,1,0,0);
-    this->normals[2] = vec4(0,1,0,0);
-    this->normals[3] = vec4(0,1,0,0);
-    this->normals[4] = vec4(0,1,0,0);
-    this->normals[5] = vec4(0,1,0,0);
-
+    this->points[4] = v3;
+    this->points[5] = v2;
+    this->normals[0] = vec4(0,1,0,1);
+    this->normals[1] = vec4(0,1,0,1);
+    this->normals[2] = vec4(0,1,0,1);
+    this->normals[3] = vec4(0,1,0,1);
+    this->normals[4] = vec4(0,1,0,1);
+    this->normals[5] = vec4(0,1,0,1);
     this->texCoord[0] = vec2(0,0);
     this->texCoord[1] = vec2(1,0);
     this->texCoord[2] = vec2(0,1);
     this->texCoord[3] = vec2(1,1);
-    this->texCoord[4] = vec2(1,0);
-    this->texCoord[5] = vec2(0,1);
+    this->texCoord[4] = vec2(0,1);
+    this->texCoord[5] = vec2(1,0);
 
     this->material = new Material();
-
+    this->Index = 6;
 }
 
 Plane::~Plane(){
